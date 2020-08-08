fcef2d568a0a54fb22b4ff9cb0d8241bbbef716c
Aparecida Geovana <aparecida.geovanna483@gmail.com>
2020-08-08 16:56:09 -0300

    teste de commit

---
+++ alurapic/src/components/shared/imagem-responsiva/imagemResponsiva.vue
@@ -0,0 +1,19 @@
+<template>
+  <img id="imagem-responsiva" :src="url" :alt="titulo">
+</template>
+
+<script>
+export default {
+
+  props: ['url', 'titulo']
+
+}
+
+</script>
+
+<style scoped>
+
+#imagem-responsiva {
+  width: 100%;
+}
+</style>
